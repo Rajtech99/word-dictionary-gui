6a479ffb42eb49d72296ae914d70dbbd5d1b27a2
Raj Manna <raj2021manna@gmail.com>
2023-06-13 21:19:24 +0530

    before add try and except

--- gui.py
+++ gui.py
@@ -14,8 +14,11 @@ def playSound():
         p = vlc.MediaPlayer(url)
         p.play()
     else:
-        messagebox.showwarning(title="Play Error", message="Audio not found!")
+        messagebox.showwarning(title="Warning", message="Audio not available!")
 
+
+def Exit():
+    win.destroy()
 
 # Get text entry value
 def info():
@@ -25,7 +28,7 @@ def info():
 
     # If the input word is a single letter
     if 0 < len(input_text) < 2:
-        messagebox.showerror(title="Error", message=f"{input_text} is not a meaningful word")
+        messagebox.showerror(title="Error", message=f"{input_text.capitalize()} is not a meaningful word")
 
     # Check the entry box empty or not
     elif input_text != '':
@@ -35,6 +38,8 @@ def info():
         code = get_value.status_code
         # If the input word is a meaningful word
         if code == 200:
+            win.geometry("400x690")
+            close_button.destroy()
             saveData("data.json", data)
             value = loadFile("data.json")
             # Get the all required items
@@ -43,33 +48,39 @@ def info():
             synonyms = value[3]
 
             # Display the meaning of the inputted word
-            Label(win, text=meaning, font=('calibre', 10, 'normal'), bg="gray90", wraplength=330, fg="blue").place(x=40, y=335, height=100, width=330)
+            Label(win, text="Meaning", font=("Terminal", 15, "bold"), bg="gray81").place(x=40, y=300, height=27,
+                                                                                         width=100)
+            Label(win, text=meaning, font=('calibre', 10, 'normal'), bg="gray90", wraplength=330).place(x=40, y=335, height=100, width=330)
             # Sound play button and label
+            Label(win, text="Audio", font=("Terminal", 15, "bold"), bg="gray81").place(x=40, y=445, height=27)
             Label(win, text=f"Say:", font=('calibre', 12, 'normal'), bg="gray81", fg="green").place(x=40, y=477, height=25, width=40)
             Button(win, text="Play", font=('calibre', 12, 'normal'), bg="gray67", fg="black", command=playSound).place(x=90, y=477, height=25, width=100)
 
             # Check the no. of items in synonyms
             if len(synonyms) != 0:
                 synonyms = ', '.join(map(str, synonyms))
-                Label(win, text=synonyms, font=('calibre', 10, 'normal'), bg="gray90", wraplength=330,
-                          fg="red").place(x=40, y=550, height=80, width=330)
+                Label(win, text="Synonyms", font=("Terminal", 15, "bold"), bg="gray81").place(x=40, y=512, height=27)
+                Label(win, text=synonyms, font=('calibre', 12, 'normal'), bg="gray90", wraplength=330).place(x=40, y=550, height=80, width=330)
+                Button(win, text="Close", bg="gray67", fg="black", command=Exit).place(x=100, y=640, width=200, height=30)
             else:
-                Label(win, text="Synonyms not available", font=('calibre', 10, 'normal'), bg="gray90", wraplength=330,
-                      fg="red").place(x=40, y=550, height=60, width=330)
+                Label(win, text="Synonyms", font=("Terminal", 15, "bold"), bg="gray81").place(x=40, y=512, height=27)
+                Label(win, text="Synonyms not available", font=('calibre', 12, 'normal'), bg="gray90", wraplength=330).place(x=40, y=550, height=60, width=330)
+                Button(win, text="Close", bg="gray67", fg="black", command=Exit).place(x=100, y=640, width=200, height=30)
+
         else:
-            messagebox.showerror(title="Error occurred", message="This word meaning is not define in US English")
+            messagebox.showerror(title="Error occurred", message="This word is not define in US English")
     else:
         messagebox.showwarning(title="Warning", message="Please enter a valid word")
 
 
 # Creating main window
 def mainWindow():
-    global word_entry, win
+    global word_entry, win, close_button
 
     # Create the main window
     win = Tk()
     win.config(bg="gray81")
-    win.geometry("400x660")
+    win.geometry("400x360")
     win.title("WordBook")
     win.resizable(False, False)
 
@@ -92,11 +103,9 @@ def mainWindow():
     # Submit button
     Button(win, text="Get Meaning", bg="gray67", fg="black", command=info).place(x=100, y=250, width=200,
                                                                                  height=30)
-
-    # heading for meaning, audio and synonyms
-    Label(win, text="Meaning", font=("Terminal", 15, "bold"), bg="gray81").place(x=40, y=300, height=27, width=100)
-    Label(win, text="Audio", font=("Terminal", 15, "bold"), bg="gray81").place(x=40, y=445, height=27)
-    Label(win, text="Synonyms", font=("Terminal", 15, "bold"), bg="gray81").place(x=40, y=512, height=27)
+    # Exit button
+    close_button = Button(win, text="Close", bg="gray67", fg="black", command=Exit)
+    close_button.place(x=100, y=300, width=200, height=30)
 
     win.mainloop()
 
