10872fe2f9ecc81a46b1ed1e94e9bed13a3b83f9
Raj Manna <raj2021manna@gmail.com>
2023-06-13 15:36:05 +0530

    third commit

--- gui.py
+++ gui.py
@@ -6,8 +6,10 @@ from main import *
 import vlc
 
 
+# Play the input text
 def playSound():
     url = sound
+    # Check this url is exists or not
     if url != '':
         p = vlc.MediaPlayer(url)
         p.play()
@@ -18,24 +20,44 @@ def playSound():
 # Get text entry value
 def info():
     global sound
+    # Get the value of input text entry
     input_text = word_entry.get()
-    if input_text != '':
+
+    # If the input word is a single letter
+    if 0 < len(input_text) < 2:
+        messagebox.showerror(title="Error", message=f"{input_text} is not a meaningful word")
+
+    # Check the entry box empty or not
+    elif input_text != '':
         url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{input_text}"
         get_value = requests.get(url)
         data = get_value.json()
         code = get_value.status_code
+        # If the input word is a meaningful word
         if code == 200:
             saveData("data.json", data)
             value = loadFile("data.json")
-            word = value[0]
+            # Get the all required items
             sound = value[1]
             meaning = value[2]
             synonyms = value[3]
-            Label(win, text=meaning, font=('calibre', 10, 'normal'), bg="white", wraplength=330, fg="red").place(x=40, y=335, height=100, width=330)
-            Label(win, text=f"Say {input_text}:", font=('calibre', 12, 'normal'), bg="gray81", wraplength=100, fg="green").place(x=40, y=477, height=25)
-            Button(win, text="Play", font=('calibre', 10, 'normal'), bg="gray67", fg="black", command=playSound).place(x=140, y=477, height=25)
+
+            # Display the meaning of the inputted word
+            Label(win, text=meaning, font=('calibre', 10, 'normal'), bg="gray90", wraplength=330, fg="blue").place(x=40, y=335, height=100, width=330)
+            # Sound play button and label
+            Label(win, text=f"Say:", font=('calibre', 12, 'normal'), bg="gray81", fg="green").place(x=40, y=477, height=25, width=40)
+            Button(win, text="Play", font=('calibre', 12, 'normal'), bg="gray67", fg="black", command=playSound).place(x=90, y=477, height=25, width=100)
+
+            # Check the no. of items in synonyms
+            if len(synonyms) != 0:
+                synonyms = ', '.join(map(str, synonyms))
+                Label(win, text=synonyms, font=('calibre', 10, 'normal'), bg="gray90", wraplength=330,
+                          fg="red").place(x=40, y=550, height=80, width=330)
+            else:
+                Label(win, text="Synonyms not available", font=('calibre', 10, 'normal'), bg="gray90", wraplength=330,
+                      fg="red").place(x=40, y=550, height=60, width=330)
         else:
-            messagebox.showerror(title="Error occurred", message="This word is not define in US English")
+            messagebox.showerror(title="Error occurred", message="This word meaning is not define in US English")
     else:
         messagebox.showwarning(title="Warning", message="Please enter a valid word")
 
@@ -43,10 +65,13 @@ def info():
 # Creating main window
 def mainWindow():
     global word_entry, win
+
+    # Create the main window
     win = Tk()
     win.config(bg="gray81")
-    win.geometry("400x600")
+    win.geometry("400x660")
     win.title("WordBook")
+    win.resizable(False, False)
 
     # Main heading label
     Label(win, text="Welcome to WordBook", font=("Terminal", 18, "bold"), fg="black", bg="gray81").pack(pady=8)
@@ -58,13 +83,20 @@ def mainWindow():
     label = Label(image_frame, image=img, bg="gray81")
     label.pack()
 
+    Label(win, text="Powered by RT Production", font=("Terminal", 10, "bold"), bg="gray81").place(x=100, y=170, width=200)
+
+    # Place the text entry box
     word_entry = Entry(win, font=('calibre', 15, 'normal'))
     word_entry.place(x=40, y=200, width=320, height=35)
 
+    # Submit button
     Button(win, text="Get Meaning", bg="gray67", fg="black", command=info).place(x=100, y=250, width=200,
                                                                                  height=30)
+
+    # heading for meaning, audio and synonyms
     Label(win, text="Meaning", font=("Terminal", 15, "bold"), bg="gray81").place(x=40, y=300, height=27, width=100)
     Label(win, text="Audio", font=("Terminal", 15, "bold"), bg="gray81").place(x=40, y=445, height=27)
+    Label(win, text="Synonyms", font=("Terminal", 15, "bold"), bg="gray81").place(x=40, y=512, height=27)
 
     win.mainloop()
 
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import json
 
 
+# Define a function for save the json data
 def saveData(file, content):
     with open(file, "w") as f:
         f.write(json.dumps(content))
 
 
+# Define a function for load all data from data.json
 def loadFile(file):
     with open(file) as f:
         item = json.load(f)
     word = item[0]['word']
-    sound = item[0]['phonetics'][0]['audio']
     meaning = item[0]['meanings'][0]['definitions'][0]['definition']
     synonyms = item[0]['meanings'][0]['synonyms']
+    sound = item[0]['phonetics'][0]['audio']
     return word, sound, meaning, synonyms
-
